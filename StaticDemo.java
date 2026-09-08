2f75ef4f05939ee318c9efdef2d27da04d2baafa
Vaishnavi Agarwal <vaishnaviagarwal145@gmail.com>
2026-09-08 12:46:32 +0530

    Added Java learning files

---
+++ StaticDemo.java
@@ -0,0 +1,10 @@
+public class StaticDemo {
+    static int a=10;
+    int b=20;
+    static class Inner{
+        static void display(){
+            System.out.println("Inside static inner class");
+        }
+    }
+    
+}

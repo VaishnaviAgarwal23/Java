d6239d325edb3b9e7ab875115140be99fc301a2e
Vaishnavi Agarwal <vaishnaviagarwal145@gmail.com>
2026-09-18 12:01:15 +0530

    Added HashSet1 program

---
+++ HashSet1.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class HashSet1 {
+
+    public static boolean registerStudent(Set<Integer> ids, int id) {
+        // Write your code
+        return ids.add(id);
+    }
+
+    public static boolean isRegistered(Set<Integer> ids, int id) {
+        // Write your code
+        return ids.contains(id);
+    }
+
+    public static int totalStudents(Set<Integer> ids) {
+        // Write your code
+        return ids.size();
+    }
+
+    public static void main(String[] args) {
+
+        Set<Integer> ids = new HashSet<>();
+
+        System.out.println(registerStudent(ids, 101));
+        System.out.println(registerStudent(ids, 102));
+        System.out.println(registerStudent(ids, 103));
+        System.out.println(registerStudent(ids, 101));
+
+        System.out.println("Registered: "
+                + isRegistered(ids, 102));
+
+        System.out.println("Total Students: "
+                + totalStudents(ids));
+    }
+}
